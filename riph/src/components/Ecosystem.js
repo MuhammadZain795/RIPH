66c2c27d207a1de25b025ff4f8c30d1d413e362d
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2021-05-11 01:23:14 +0500

    Ecosystem component completed.

--- riph/src/components/Ecosystem.js
+++ riph/src/components/Ecosystem.js
@@ -5,15 +5,43 @@ export default function Ecosystem() {
     return (
         <Container>
             <MonkeySide>
-                <Monkey src="/images/RIPH Coin.png"/>
-                <CGaming src="/images/Blank Gold Circle.png"/>
-                <CStaking src="/images/Blank Gold Circle.png"/>
-                <CNFTs src="/images/Blank Gold Circle.png"/>
-                <CPayments src="/images/Blank Gold Circle.png"/>
-                <CStreaming src="/images/Blank Gold Circle.png"/>
+                <Monkey>
+                    <img src="/images/RIPH Coin.png"/>
+                </Monkey>
+                <CGaming>
+                    <img src="/images/Blank Gold Circle.png"/>
+                    <h1>Gaming</h1>
+                </CGaming>
+                <CStaking>
+                    <img src="/images/Blank Gold Circle.png"/>
+                    <h1>Staking</h1>
+                </CStaking>
+                <CNFTs>
+                    <img src="/images/Blank Gold Circle.png"/>
+                    <h1>NFTs</h1>
+                </CNFTs>
+                <CPayments>
+                    <img src="/images/Blank Gold Circle.png"/>
+                    <h1>Payments</h1>
+                </CPayments>
+                <CStreaming>
+                    <img src="/images/Blank Gold Circle.png"/>
+                    <h1>Streaming</h1>
+                </CStreaming>
             </MonkeySide>
             <TextSide>
-                Text
+                <h1>We' re Building an Ecosystem</h1>
+                <p>As we grow, so does RIPH.</p>
+                <p>As we expand into new field, developing new
+                    projects, we provide RIPH with more utility which
+                    in turns gives it more value.</p>
+                <p>The utilisation of RIPH will also be helping in the
+                    conservation of gorillas. Automatically donating
+                    1% of all transactions of charity.
+                </p>
+                <p>We're at the beginning of an exciting journey with
+                    an already incredibly supportive community.
+                </p>
             </TextSide>
         </Container>
     )
@@ -28,59 +56,119 @@ const Container = styled.div`
 `
 const MonkeySide = styled.div`
     width: 50%;
-    color: white;
-    /* display: flex;
-    justify-content: center; */
+    color: rgb(207,181,59);
 `
 const TextSide = styled.div`
     color: white;
     width: 50%;
+    padding: 6vw;
+    letter-spacing: 1.4px;
+    p{
+        font-size: 1vw;
+        padding-top: 1vw;
+    }
+    h1{
+        font-size: 2vw;
+    }
 `
-const Monkey = styled.img`
-    width: 13vw;
-    height: 13vw;
+const Monkey = styled.div`
     position: absolute;
-    padding-top: 10vw;
-    padding-left: 18vw;
     z-index:1;
+    img{
+        position: absolute;
+        width: 13vw;
+        height: 13vw;
+        padding-top: 10vw;
+        padding-left: 18vw;
+    }
 `
-const CPayments = styled.img`
-    width: 6vw;
-    height: 6vw;
+const CPayments = styled.div`
     position: absolute;
-    z-index:2;
-    padding-top: 14vw;
-    padding-left: 35vw;
+    z-index:1;
+    img{
+        position: absolute;
+        width: 6vw;
+        height: 6vw;
+        padding-top: 14vw;
+        padding-left: 34vw;
+    }
+    h1{
+        position: absolute;
+        padding-top: 15.7vw;
+        padding-left: 34.9vw;
+        font-size: 0.8vw;
+        text-transform: uppercase;
+    }
 `
-const CGaming = styled.img`
-    width: 6vw;
-    height: 6vw;
+const CGaming = styled.div`
     position: absolute;
-    z-index:3;
-    padding-top: 8vw;
-    padding-left: 10vw;
+    z-index:1;
+    img{
+        position: absolute;
+        width: 6vw;
+        height: 6vw;
+        padding-top: 5vw;
+        padding-left: 12vw;
+    }
+    h1{
+        position: absolute;
+        padding-top: 6.5vw;
+        padding-left: 13vw;
+        font-size: 1vw;
+        text-transform: uppercase;
+    }
 `
-const CStaking = styled.img`
-    width: 7vw;
-    height: 7vw;
+const CStaking = styled.div`
     position: absolute;
-    z-index:4;
-    padding-top: 4vw;
-    padding-left: 30vw;
+    z-index:1;
+    img{
+        position: absolute;
+        width: 7vw;
+        height: 7vw;
+        padding-top: 3vw;
+        padding-left: 33vw;
+    }
+    h1{
+        position: absolute;
+        padding-top: 4.8vw;
+        padding-left: 34.7vw;
+        font-size: 1vw;
+        text-transform: uppercase;
+    }
 `
-const CStreaming = styled.img`
-    width: 5vw;
-    height: 5vw;
+const CStreaming = styled.div`
     position: absolute;
-    z-index:5;
-    padding-top: 25vw;
-    padding-left: 28vw;
+    z-index:1;
+    img{
+        position: absolute;
+        width: 6vw;
+        height: 6vw;
+        padding-top: 23vw;
+        padding-left: 27vw;
+    }
+    h1{
+        position: absolute;
+        padding-top: 24.7vw;
+        padding-left: 27.9vw;
+        font-size: 0.7vw;
+        text-transform: uppercase;
+    }
 `
-const CNFTs = styled.img`
-    width: 7vw;
-    height: 7vw;
+const CNFTs = styled.div`
     position: absolute;
-    z-index:6;
-    padding-top: 20vw;
-    padding-left: 10vw;
+    z-index:1;
+    img{
+        position: absolute;
+        width: 7vw;
+        height: 7vw;
+        padding-top: 23vw;
+        padding-left: 10vw;
+    }
+    h1{
+        position: absolute;
+        padding-top: 25vw;
+        padding-left: 12vw;
+        font-size: 1vw;
+        text-transform: uppercase;
+    }
 `
